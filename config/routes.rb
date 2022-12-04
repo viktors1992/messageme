c88080e257106b991dd2039f9fb9287027ffdbbf
viktors1992 <v.nilovs@gmail.com>
2022-12-04 18:20:57 +0200

    login and index controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root 'chatroom#index'
+  get 'login', to: 'sessions#new'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
